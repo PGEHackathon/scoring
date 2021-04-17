43916efbafd860baff89b49c3f438e3e88d97afd
John T. Foster <johntfosterjr@gmail.com>
2021-04-17 12:05:14 -0500

    Add presentation and code review scoring

--- score.py
+++ score.py
@@ -10,6 +10,7 @@ from jinja2 import Template
 import os
 import subprocess
 import matplotlib
+import json
 
 pgf_with_latex = {"pgf.texsystem": 'pdflatex'}
 matplotlib.rcParams.update(pgf_with_latex)
@@ -122,14 +123,19 @@ latex_jinja_env = jinja2.Environment(
         loader = jinja2.FileSystemLoader(os.path.abspath('.'))
 )
 
-def create_team_report(team_name, mse, goodness_score):
+def create_team_report(team_name, mse, 
+                       goodness_score, 
+                       presentation_comments,
+                       code_review_comments):
 
     template = latex_jinja_env.get_template('report_template.tex')
 
     #Render tex and write to file
-    rendered_tex = template.render(teamname=team_name, 
+    rendered_tex = template.render(teamname=team_name.replace('_', ' '), 
                                    mse=mse,
-                                   goodness=goodness_score)
+                                   goodness=goodness_score,
+                                   presentationComments=presentation_comments,
+                                   codereviewComments=code_review_comments)
     tex_filename = f'{team_name}_report.tex'
     with open(tex_filename, 'w') as f:
         f.write(rendered_tex)
@@ -139,7 +145,49 @@ def create_team_report(team_name, mse, goodness_score):
                     '-output-directory=reports', tex_filename])
     return
 
+def parse_team_name(team_name):
+    return team_name.replace(' ', '').replace('-', '').replace('_', '').lower()
 
+def parse_question_scores(score):
+    return int(score.split(',')[0])
+
+def get_presentation_dataframes(presentation_csv):
+
+    df = pd.read_csv(presentation_csv)
+    df = df.drop([0,1])
+    df = df.astype('str')
+    df['Q1'] = df['Q1'].apply(parse_team_name)
+    df['Q2'] = df['Q2'].apply(parse_question_scores)
+    df['Q3'] = df['Q3'].apply(parse_question_scores)
+    df['Q4'] = df['Q4'].apply(parse_question_scores)
+    df['Q5'] = df['Q5'].apply(parse_question_scores)
+    df['Q6'] = df['Q6'].apply(parse_question_scores)
+    df['Q7'] = df['Q7'].apply(parse_question_scores)
+    df['Q8'] = df['Q8'].apply(parse_question_scores)
+
+    scores_df = df.loc[:, 'Q1':'Q8'].groupby('Q1').mean().sum(axis=1)
+    comments_df = df[['Q1','Q9']].groupby('Q1')['Q9'].apply(list)
+
+    return scores_df, comments_df
+
+def get_code_review_dataframes(code_review_csv):
+
+    df = pd.read_csv(code_review_csv)
+    df = df.drop([0,1])
+    df = df.astype('str')
+    df['Q1'] = df['Q1'].apply(parse_team_name)
+    df['Q2'] = df['Q2'].apply(parse_question_scores)
+    df['Q3'] = df['Q3'].apply(parse_question_scores)
+    df['Q4'] = df['Q4'].apply(parse_question_scores)
+    df['Q5'] = df['Q5'].apply(parse_question_scores)
+    df['Q6'] = df['Q6'].apply(parse_question_scores)
+
+    scores_df = df.loc[:, 'Q1':'Q6'].groupby('Q1').mean().sum(axis=1)
+    comments_df = df[['Q1','Q7']].groupby('Q1')['Q7'].apply(list)
+
+    return scores_df, comments_df
+
+# %%
 
 if __name__ == '__main__':
 
@@ -184,19 +232,52 @@ if __name__ == '__main__':
                                                                    solution_array)
                 team_goodness_score.append(goodness_score)
 
-                create_team_report(team_name, mse, goodness_score)
+                presentation_score_df, presentation_comments_df = \
+                    get_presentation_dataframes('presentation.csv')
+
+                code_review_score_df, code_review_comments_df = \
+                    get_code_review_dataframes('code_review.csv')
+
+
+                try:
+                    presentation_comments = \
+                        presentation_comments_df[parse_team_name(team_name)]
+                    code_review_comments = \
+                        code_review_comments_df[parse_team_name(team_name)]
+                except:
+                    presentation_comments = ["None"]
+                    code_review_comments = ["None"]
+
+
+                create_team_report(team_name, mse, 
+                                   goodness_score, 
+                                   presentation_comments,
+                                   code_review_comments)
 
     df = pd.DataFrame(np.array([team_names, 
                                 team_mse, 
                                 team_goodness_score]).T, columns=['Team Names',
                                                                   'MSE',
                                                                   'Goodness Score'])
+    df['Short Names'] = df['Team Names'].apply(parse_team_name)
+    df.set_index(['Short Names'], inplace=True)
+
+    df['Pres. Score'] = presentation_score_df
+    df['Code Score'] = code_review_score_df
     df['MSE Rank'] = df['MSE'].astype('float64').rank(method='min', ascending=True).astype('int')
-    df['Goodness Score Rank'] = df['Goodness Score'].astype('float64').rank(method='min', ascending=False).astype('int')
-    df['Overall Rank'] = (df['MSE Rank'].astype('float64') + df['Goodness Score Rank'].astype('float64')).rank(method='min').astype('int')
+    df['Goodness Rank'] = df['Goodness Score'].astype('float64').rank(method='min', ascending=False).astype('int')
+    df['Pres. Rank'] = df['Pres. Score'].astype('float64').rank(method='min', ascending=False).astype('int')
+    df['Code Rank'] = df['Code Score'].astype('float64').rank(method='min', ascending=False).astype('int')
+
+    df['Overall Rank'] = (df['MSE Rank'].astype('float64') + 
+                          df['Goodness Rank'].astype('float64') + 
+                          df['Pres. Rank'].astype('float64') + 
+                          df['Code Rank'].astype('float64')
+                         ).rank(method='min').astype('int')
 
     df.sort_values('Overall Rank', inplace=True)
-
+    df.index.name = None
+    #df.reset_index(inplace=True)
     with open('final_rankings_table.tex', 'w') as f:
         df.to_latex(index=False, buf=f)
 
