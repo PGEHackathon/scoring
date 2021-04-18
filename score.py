a744c7a0f9e8de4f1d4cd4c15877b45383836494
John T. Foster <johntfosterjr@gmail.com>
2021-04-18 14:20:07 -0500

    Update score.py

--- score.py
+++ score.py
@@ -204,7 +204,7 @@ if __name__ == '__main__':
                     'PGEHackathon/scoring', 'PGEHackathon/PGEHackathon', 
                     'PGEHackathon/resources', 'PGEHackathon/TheNomads', 
                     'PGEHackathon/truth_data', 'PGEHackathon/fooled-by-randomness', 
-                    'PGEHackathon/404_Not_Found']
+                    'PGEHackathon/404_Not_Found', 'PGEHackathon/ripROACH']
 
     solution_array = np.load('True_for_predrill_3yr.npy') # Solution
 
