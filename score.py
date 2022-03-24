a1f564d73facbce2acfe002e3a18becfc3f8e10a
John T. Foster <johntfosterjr@gmail.com>
2022-03-24 15:01:41 -0500

    Update files

--- score.py
+++ score.py
@@ -16,15 +16,16 @@ import eclipse
 pgf_with_latex = {"pgf.texsystem": 'pdflatex'}
 matplotlib.rcParams.update(pgf_with_latex)
 
-def create_accuracy_plot_and_return_mse(prediction_df, solution_array):
-    prediction_array = prediction_df['Prediction, MSTB'].to_numpy()
+def create_accuracy_plot_and_return_mse_and_cumulative_prod(result_df):
+    prediction_array = result_df['2-Year Cumulative Production (bbl)'].to_numpy()
+    solution_array = result_df['Total_Prod_Well'].to_numpy()
     plt.figure(figsize = (6,4))
     plt.plot(solution_array, prediction_array,'o',label = 'Estimates')
-    plt.plot([500,2000],[500,2000],'--r',label = '1:1 line')
+    plt.plot([20000,150000],[20000,150000],'--r',label = '1:1 line')
     plt.plot([solution_array[0], solution_array[0]],
              [prediction_array[0], solution_array[0]],
              '--',color = 'gray',label = 'misfit')
-    for i in range(1,10):
+    for i in range(1,2):
         plt.plot([solution_array[i], solution_array[i]],
                  [prediction_array[i], solution_array[i]],
                  '--',color = 'gray')
@@ -32,85 +33,12 @@ def create_accuracy_plot_and_return_mse(prediction_df, solution_array):
     plt.ylabel('Prediction, MSTB'); 
     plt.grid('on'); 
     plt.legend(); 
-    plt.axis([500,2000,500,2000])
+    plt.axis([20000,150000,20000, 150000])
     plt.savefig('accuracy.pgf')
-    return np.round(mean_squared_error(solution_array,prediction_array),3)
+    mse = np.round(mean_squared_error(solution_array,prediction_array),3)
+    total_prod = result_df['Total_Prod_Field'].to_numpy()[0]
+    return mse, total_prod
 
-def create_realizations_plots(prediction_df, solution_array):
-    prediction_realizations = prediction_df.iloc[:,2:].to_numpy()
-    kde = [scipy.stats.gaussian_kde(prediction_realizations[i], 
-                                    bw_method = None) for i in range(10)]
-    t_range = [np.linspace(prediction_realizations[i].min() * 0.8,
-                           prediction_realizations[i].max() * 1.2, 200) for i in range(10)]
-    
-    plt.figure(figsize =(6,8))
-    for i in range(10):
-        ax = plt.subplot(5,2,i+1)
-        if i == 0:
-            pdf, = ax.plot(t_range[i],kde[i](t_range[i]), lw=2, label=f'PDF of reals')
-            real, = ax.plot(solution_array[i], 0,'ro',markersize = 10, label ='True')
-            plt.xlabel('Prediction, MSTB');
-            plt.title(f'Preproduction No. {74+i}')
-        else:
-            pdf, = ax.plot(t_range[i],kde[i](t_range[i]), lw=2)
-            real, = ax.plot(solution_array[i], 0, 'ro',markersize = 10)
-            plt.title(f'Preproduction No. {74+i}')
-        if i == 1:
-            plt.legend([pdf, real], ['PDF of realz.', 'True'], bbox_to_anchor=(1.05, 1.0), loc='upper left')
-        if i % 2 == 0:
-            plt.ylabel('Probability')
-
-    plt.tight_layout()
-    plt.savefig('realizations.pgf')
-    return
-
-def compute_goodness_array(prediction_df, solution_array):
-    prediction_realizations = prediction_df.iloc[:,2:].to_numpy()
-    goodness_score = []
-    list_percentile_lower = [50-5*i for i in range(0,11)]   # Define upper/lower boundary of "within-percentile" ranges
-    list_percentile_upper = [50+5*i for i in range(0,11)]   # E.g., 10% range - from 45% to 55% percentile
-
-    for i in range(11):     # 0%, 10%, 20%, 30%, ... 100% percentiles ranges
-        num_within = 0      # Counts for wells within the range
-        for j in range(10): # 10 Predrill wells
-            min_ = np.percentile(prediction_realizations[j],list_percentile_lower[i])
-            max_ = np.percentile(prediction_realizations[j],list_percentile_upper[i])
-            print(solution_array[j])
-            if solution_array[j] > min_ and solution_array[j] < max_:
-                num_within += 1
-        goodness_score.append(num_within)
-    return goodness_score
-
-def create_goodness_plot_and_return_goodness_score(prediction_df, solution_array):
-    goodness_score = compute_goodness_array(prediction_df, solution_array)
-    prediction_realizations = prediction_df.iloc[:,2:].to_numpy()
-
-    plt.figure(figsize = (6,4))
-    plt.plot(goodness_score,'--ko', label = 'Goodness')
-    plt.plot([0,10],[0,10], '-r', label = '1:1 line')
-    #plt.fill([i for i in range(11)],goodness_score,alpha = 0.2, label = 'misfit area')
-    plt.xticks([i for i in np.linspace(0,10,6)], [f'{np.int(i*10)}%' for i in np.linspace(0,10,6)])
-    plt.yticks([i for i in np.linspace(0,10,6)], [f'{np.int(i*10)}%' for i in np.linspace(0,10,6)])
-    plt.xlabel('Within percentile'); 
-    plt.ylabel('Percentage of wells within the range')
-    plt.legend(); 
-    plt.savefig('goodness.pgf')
-
-    ## Total area of plot is 100 (square of 10 x 10)
-    # If goodness plot perfectly along with 1:1 line, that should be "1" 
-    # If goodness plot all flat at 0% in y-axis, that should be "0" 
-    # If goodness plot all flat at 100% in y-axis, that should be "0.5" 
-    # Follow lines of code compute and normalize area above/below area to get goodness score (0~1)
-    goodness_score_upNdown = np.array(goodness_score) - np.arange(0,11) 
-    a_interval_index = [1 if goodness_score[i+1] >= i+1 else 0 for i in range(10)]
-    goodness_score_ = 1
-    for i in range(10):
-        if a_interval_index[i] == 1:
-            goodness_score_ -= +1/2*goodness_score_upNdown[i+1]/45
-        else:
-            goodness_score_ -= -goodness_score_upNdown[i+1]/55
-
-    return np.abs(np.round(goodness_score_,3))
 
 latex_jinja_env = jinja2.Environment(
     block_start_string = '\BLOCK{',
@@ -127,7 +55,7 @@ latex_jinja_env = jinja2.Environment(
 )
 
 def create_team_report(team_name, mse, 
-                       goodness_score, 
+                       total_prod, 
                        presentation_comments,
                        code_review_comments):
 
@@ -136,7 +64,7 @@ def create_team_report(team_name, mse,
     #Render tex and write to file
     rendered_tex = template.render(teamname=team_name.replace('_', ' '), 
                                    mse=mse,
-                                   goodness=goodness_score,
+                                   total_prod=total_prod,
                                    presentationComments=presentation_comments,
                                    codereviewComments=code_review_comments)
     tex_filename = f'{team_name}_report.tex'
@@ -209,85 +137,83 @@ if __name__ == '__main__':
 
     team_names = []
     team_mse = []
-    team_goodness_score = []
+    team_total_prod = []
     for repo in repos:
 
-
         if repo not in blocked_list:
-            print(f"Moving submission file for: {repo}")
 
+            print(f"Collecting submission file for: {repo}")
             result = gh.get_file_in_repo('scoring/submission/solution.csv', repo)
 
-            if result is not None:
+            team_name = repo.split('/')[1]
 
-                team_name = repo.split('/')[1]
-                team_names.append(team_name)
+            if result is not None:
 
                 submission = pd.read_csv(StringIO(result))
                 submission.to_csv(f'submissions/{team_name}_solution.csv')
 
+                print(f"Getting results file for: {repo}")
+                result = gh.get_file_in_repo(f'{team_name}_result.csv', 'PGEHackathon/simulation_results')
 
-                # prediction_df = pd.read_csv(StringIO(result))
+                if result is not None:
 
-                # mse = create_accuracy_plot_and_return_mse(prediction_df, 
-                                                          # solution_array)
-                # team_mse.append(mse)
+                    team_names.append(team_name)
 
-                # create_realizations_plots(prediction_df, solution_array)
+                    result_df = pd.read_csv(StringIO(result))
 
-                # goodness_score = \
-                    # create_goodness_plot_and_return_goodness_score(prediction_df,
-                                                                   # solution_array)
-                # team_goodness_score.append(goodness_score)
-
-                # presentation_score_df, presentation_comments_df = \
-                    # get_presentation_dataframes('presentation.csv')
-
-                # code_review_score_df, code_review_comments_df = \
-                    # get_code_review_dataframes('code_review.csv')
+                    mse, total_prod = create_accuracy_plot_and_return_mse_and_cumulative_prod(result_df)
+                    team_mse.append(mse)
+                    team_total_prod.append(total_prod)
 
 
-                # try:
-                    # presentation_comments = \
-                        # presentation_comments_df[parse_team_name(team_name)]
-                    # code_review_comments = \
-                        # code_review_comments_df[parse_team_name(team_name)]
-                # except:
-                    # presentation_comments = ["None"]
-                    # code_review_comments = ["None"]
+                    presentation_score_df, presentation_comments_df = \
+                        get_presentation_dataframes('presentation.csv')
+
+                    code_review_score_df, code_review_comments_df = \
+                        get_code_review_dataframes('code_review.csv')
 
 
-                # create_team_report(team_name, mse, 
-                                   # goodness_score, 
-                                   # presentation_comments,
-                                   # code_review_comments)
+                    try:
+                        presentation_comments = \
+                            presentation_comments_df[parse_team_name(team_name)]
+                        code_review_comments = \
+                            code_review_comments_df[parse_team_name(team_name)]
+                    except:
+                        presentation_comments = ["None"]
+                        code_review_comments = ["None"]
 
-    # df = pd.DataFrame(np.array([team_names, 
-                                # team_mse, 
-                                # team_goodness_score]).T, columns=['Team Names',
-                                                                  # 'MSE',
-                                                                  # 'Goodness Score'])
-    # df['Short Names'] = df['Team Names'].apply(parse_team_name)
-    # df.set_index(['Short Names'], inplace=True)
 
-    # df['Pres. Score'] = presentation_score_df
-    # df['Code Score'] = code_review_score_df
-    # df['MSE Rank'] = df['MSE'].astype('float64').rank(method='min', ascending=True, na_option='top')
-    # df['Goodness Rank'] = df['Goodness Score'].astype('float64').rank(method='min', ascending=False, na_option='top')
-    # df['Pres. Rank'] = df['Pres. Score'].astype('float64').rank(method='min', ascending=False, na_option='top')
-    # df['Code Rank'] = df['Code Score'].astype('float64').rank(method='min', ascending=False, na_option='top')
+                    create_team_report(team_name, mse, 
+                                       total_prod, 
+                                       presentation_comments,
+                                       code_review_comments)
 
-    # df['Overall Rank'] = (df['MSE Rank'].astype('float64') + 
-                          # df['Goodness Rank'].astype('float64') + 
-                          # df['Pres. Rank'].astype('float64') + 
-                          # df['Code Rank'].astype('float64')
-                         # ).rank(method='min')
+    df = pd.DataFrame(np.array([team_names, 
+                                team_mse, 
+                                team_total_prod]).T, columns=['Team Names',
+                                                              'MSE',
+                                                              'Field total production'])
+    df['Short Names'] = df['Team Names'].apply(parse_team_name)
+    df.set_index(['Short Names'], inplace=True)
 
-    # df.sort_values('Overall Rank', inplace=True)
-    # df.index.name = None
-    # #df.reset_index(inplace=True)
-    # with open('final_rankings_table.tex', 'w') as f:
-        # df.to_latex(index=False, buf=f)
+    df['Pres. Score'] = presentation_score_df
+    df['Code Score'] = code_review_score_df
+    df['MSE Rank'] = df['MSE'].astype('float64').rank(method='min', ascending=True, na_option='top')
+    df['Total Production Rank'] = df['Field total production'].astype('float64').rank(method='min', ascending=False, na_option='top')
+    df['Pres. Rank'] = df['Pres. Score'].astype('float64').rank(method='min', ascending=False, na_option='top')
+    df['Code Rank'] = df['Code Score'].astype('float64').rank(method='min', ascending=False, na_option='top')
 
-    # subprocess.run(['latexmk', '-pdf', 
-                    # '-output-directory=reports', 'final_report.tex'])
+    df['Overall Rank'] = (0.3 * df['MSE Rank'].astype('float64') + 
+                          0.3 * df['Total Production Rank'].astype('float64') + 
+                          0.3 * df['Pres. Rank'].astype('float64') + 
+                          0.1 * df['Code Rank'].astype('float64')
+                         ).rank(method='min')
+
+    df.sort_values('Overall Rank', inplace=True)
+    df.index.name = None
+    #df.reset_index(inplace=True)
+    with open('final_rankings_table.tex', 'w') as f:
+        df.to_latex(index=False, buf=f)
+
+    subprocess.run(['latexmk', '-pdf', 
+                    '-output-directory=reports', 'final_report.tex'])
